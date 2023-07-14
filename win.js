e42250e7d2b9c14841f301a51f270214faaa4290
rahulk6661 <133742767+rahulk6661@users.noreply.github.com>
2023-07-14 20:07:21 +0530

    Create win.js
    js file

---
+++ win.js
@@ -0,0 +1,10 @@
+var p=document.getElementsByClassName("title");
+p[0].style.fontWeight="bold";
+p[0].style.backgroundColor="green";
+var s=document.getElementsByClassName("list-group-item");
+for(let i=0;i<s.length;i++)
+    {
+        s[i].style.fontWeight="bold";
+    }
+s[2].style.backgroundColor="green";
+

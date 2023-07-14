6fa628ea58fd5f626d4ae6bf5c101e6cecd330ca
rahulk6661 <133742767+rahulk6661@users.noreply.github.com>
2023-07-14 21:44:58 +0530

    Update win.js

--- win.js
+++ win.js
@@ -7,4 +7,13 @@ for(let i=0;i<s.length;i++)
         s[i].style.fontWeight="bold";
     }
 s[2].style.backgroundColor="green";
-
+var m=document.querySelectorAll(".list-group-item");
+m[1].style.backgroundColor="green"
+m[2].style.visibility = "hidden";
+for(let i=0;i<m.length;i++)
+    {
+        if(i%2===0)
+        {
+            m[i].style.backgroundColor="green";
+        }
+    }

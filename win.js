1bc2fa225208f4a662650f7d2153ba4648ec5418
rahulk6661 <133742767+rahulk6661@users.noreply.github.com>
2023-07-15 09:20:52 +0530

    Update win.js

--- win.js
+++ win.js
@@ -17,3 +17,42 @@ for(let i=0;i<m.length;i++)
             m[i].style.backgroundColor="green";
         }
     }
+var itemlist1=document.querySelector('#items');
+//1-parent node or parentname is just same just only change syntax
+//itemlist1.parentNode.style.backgroundColor="blue";
+itemlist1.parentElement.style.backgroundColor="yellow";
+//2-lastelementchild
+itemlist1.lastElementChild.style.backgroundColor="blue";
+//3-Lastchild
+console.log(itemlist1.lastChild);
+
+
+//5-firstelementchild
+itemlist1.firstElementChild.style.backgroundColor="orange";
+//6-firstchild
+console.log(itemlist1.firstChild);
+//7-nextsbiling
+console.log(itemlist1.nextSibling);
+//8-nextelementsibiling
+console.log(itemlist1.nextElementSibling);
+//9-previoussibiling
+console.log(itemlist1.previousSibling);
+//10-previouselemnetsibiling
+itemlist1.previousElementSibling.textContent="Awesome create";
+itemlist1.previousElementSibling.style.color="green";
+//11-createelement
+var newdiv1=document.createElement('div');
+//12-set attribute
+//1-class attribute add
+newdiv1.className="hello";
+//2-id attribute add
+newdiv1.id="hello1";
+//3-setattribute
+newdiv1.setAttribute('title','vo');
+//13-createtextnode
+var newdivtext1=document.createTextNode('hello ji');
+newdiv1.appendChild(newdivtext1);
+//14-addinng into dom
+var con=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+con1.insertbefore(newdiv,h1);
